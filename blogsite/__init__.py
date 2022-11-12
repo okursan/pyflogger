8185cb417a9ebeee03712498d044517d7ace4f06
okursan <okursan@users.noreply.github.com>
2022-11-13 00:04:51 +0300

    additions and deletions

--- blogsite/__init__.py
+++ blogsite/__init__.py
@@ -4,7 +4,7 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.sqlite"
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
@@ -19,7 +19,9 @@ def create_app():
     app.register_blueprint(blueprint=auth, url_prefix='/')
     
     from .models import User
-    
+
+    create_database(app=app)
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -29,4 +31,10 @@ def create_app():
         return User.query.get(int(id))
     
     return app
-        
+
+def create_database(app):
+    if not path.exists('blogsite/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print("deneme")
+    print('Created Database!')
--- blogsite/auth.py
+++ blogsite/auth.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
+from . import db
+from .models import User
+from flask_login import login_user, logout_user, login_required, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -6,12 +10,16 @@ auth = Blueprint('auth', __name__)
 def login():
     if request.method == 'POST':
         username = "okursan"
-        email = "omerdurmus@gmail.com"
-        password = "123456"
-        first_name = "Omer"
-        last_name = "Durmus"
+        password = generate_password_hash("123456", method='sha256')
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(user=new_user, remember=True)
+        return redirect(url_for('views.home'))
     return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
+    logout_user()
     return redirect(location=url_for('views.home'))

82f6338cec77a57df86b8984d8912c11ec6357f9
okursan <okursan@users.noreply.github.com>
2022-11-14 00:26:52 +0300

    continue

--- blogsite/views.py
+++ blogsite/views.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template
+from . import db
+from .models import User
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("home.html")
+    abc = User.query.all()
+    return render_template("home.html", abc=abc)
 
 @views.route('/<variable>')
 def variable_page(variable):

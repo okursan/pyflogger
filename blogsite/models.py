8da6eabbd2cd3c30fb93f04e9bb845267663a722
okursan <okursan@users.noreply.github.com>
2022-11-19 11:54:03 +0300

    Post model added Comment model added Database updated Dashboard template added Dashboard view added

--- blogsite/models.py
+++ blogsite/models.py
@@ -7,3 +7,17 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(25), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.String(150), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
--- blogsite/views.py
+++ blogsite/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 from . import db
 from .models import User
 
@@ -7,6 +8,11 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
 
 @views.route('/<variable>')
 def variable_page(variable):
